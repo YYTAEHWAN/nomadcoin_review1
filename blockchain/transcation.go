854e17c51c1122dee37be86db30a52f27395d8d9
YYTAEHWAN <ygen1002@gmail.com>
2022-02-08 20:27:41 +0900

    10-9 만 봤을 때의 의문들

--- blockchain/transcation.go
+++ blockchain/transcation.go
@@ -51,65 +51,41 @@ func (t *Tx) MakeTxTimestamp() {
 	t.Timestamp = int(time.Now().Unix())
 }
 
-type idAndIndexSlice struct {
-	InputTxId string
-	Index     []int
-}
-
-func (i idAndIndexSlice) appendInt(index int) {
-	i.Index = append(i.Index, index)
-}
-이렇게하면 어떻게 되죠
-// type testVar struct {
-// 	v []*idAndIndexSlice
-// }
-
 func (b *blockchain) UTxOutsByAddress(address string) []*UTxOut {
 
 	var uTxOuts []*UTxOut
-	blocks := b.Blocks()
-	creatorTx := make(map[string]bool)
-	creatorTxIndex := make(map[string]int)
+	creatorIds := make(map[string]bool)
 
-	var testVar []*idAndIndexSlice
-
-	for _, block := range blocks {
+	for _, block := range Blockchain().Blocks() {
 		for _, tx := range block.Transaction {
 			for _, input := range tx.TxIns {
 				if input.Owner == address {
-					creatorTx[input.TxId] = true
-					creatorTxIndex[input.TxId] = input.Index
+					creatorIds[input.TxId] = true
 
-					idAndIndex := &idAndIndexSlice{input.TxId, nil}
-					idAndIndex.appendInt(input.Index)
-					testVar = append(testVar, idAndIndex)
 				}
 			}
-		}
-	}
-		for _, tx := range block.Transaction {
+
 			for index, output := range tx.TxOuts {
-				if output.Owner == address { // 주소가 같으면 일단 요구하는 사람의 TxOut들이고
-					// 이제 그 TxOut이 사용됐는지 안됐는지를 확인
-					if ok := creatorTx[tx.Id]; !ok { // TxIn으로 사용된 TxOut이 있는 tx.Id 리스트를 사용하여 사용되었을 가능성이 있는지 확인
-						// 만약 이 TxOut이 존재하는 tx.Id가  사용된 TxOut tx.Id 리스트에 존재하지 않는다면 무조건 UTxOuts에 들어가야함
+				if output.Owner == address {
+					if ok := creatorIds[tx.Id]; !ok {
 						uTxOut := &UTxOut{tx.Id, index, output.Amount}
 						uTxOuts = append(uTxOuts, uTxOut)
-					} else { // 한 tx.Id 에는 같은 address로 돈을 보내는 많은 txOut이 있을 수 있고 그러므로 인덱스를 확인해야 함
-						if !(creatorTxIndex[tx.Id] == index) {
-							// 인덱스가 같지 않으면 사용되지 않은 것이므로 추가해주는 것이 맞는데
-							// 여기서 문제가 발생함 // 여기서 문제가 발생함 // 여기서 문제가 발생함 // 여기서 문제가 발생함 // 여기서 문제가 발생함 // 여기서 문제가 발생함
-							// 말했듯 한 tx.Id라도 같은 address로 돈을 보내는 많은 tx.Out이 있을 수 있기 때문에
-							// 한 tx.Id에서 사용된 txOut, 사용되지 않은 txOut을 구분해야하는데
-							// 그 구분은 index로 하면 된다고 하지만 그 인덱스를 어디에 저장하여야 할지 모르겠음
-							uTxOut := &UTxOut{tx.Id, index, output.Amount}
-							uTxOuts = append(uTxOuts, uTxOut)
-						}
+						// 이럴 경우 내가 걱정되는 건
+						// input에 사용된 txId는 output의 txId라는 건데
+						// 한 블록에 같은 주소로 여러개의 output Txs가 생성되었을 때
+						// 6개 TxOut 중 4개만 쓰이고 2개는 안쓰였다면?
+						// 그래도 이 로직에 따르면 그 2개는 spentTxOut 으로 분류될텐데 이 로직을 어떻게 고쳐야 할까
 					}
 				}
 			}
 		}
 	}
+	// 의문 2 TxIn 은 여러개의 TxOut이 모여서 만들어진다고 했는데
+	// 그럼 하나의 TxIn은 하나의 TxId, Index를 만드는데 어떻게 여러개의 TxOut을 합칠 수 있지?
+
+	// 의문 3
+	// 그럼 어떻게 해결하지?
+
 	return uTxOuts
 }
 
@@ -140,6 +116,9 @@ func makeCoinbaseTx(address string) *Tx {
 }
 
 func makeTx(from string, to string, amount int) (*Tx, error) {
+	if Blockchain().TotalBalanceByAddress(from) < amount {
+		return nil, errors.New("not enough money")
+	}
 
 }
 
